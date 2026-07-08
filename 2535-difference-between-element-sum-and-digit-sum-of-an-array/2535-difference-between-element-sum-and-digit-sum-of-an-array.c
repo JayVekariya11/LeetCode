954a89bde85627401ce4a3e727c15118a73283b3
JayVekariya11 <jayvekariya1107@gmail.com>
2026-07-08 22:15:25 +0530

    Time: 0 ms (100%), Space: 9.3 MB (16.04%) - LeetHub

---
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.c
@@ -0,0 +1,16 @@
+int differenceOfSum(int* nums, int numsSize) {
+    int elementSum = 0;
+    int digitSum = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        elementSum += nums[i];
+
+        int num = nums[i];
+        while (num != 0) {
+            digitSum += num % 10;
+            num /= 10;
+        }
+    }
+
+    return abs(elementSum - digitSum);  
+}
